a476edf5070d1444cf95e4692d8b6fa98c3ab4f0
solkaze <t.yoichi.0805@gmail.com>
2024-10-09 01:01:58 +0900

    ゴール情報の設定

--- locateTarget.py
+++ locateTarget.py
@@ -62,3 +62,27 @@ def locateEnemy(imInputHSV):
 		sSize = -1
 
 	return (sHorizontal, sVertical, sSize), imGreenBinary
+
+def locateGoal(imInputHSV):
+    # 対象物(黄色)
+	vMinHSV = np.array([20,180,0])
+	vMaxHSV = np.array([40,255,255])
+	imYellow = cv2.inRange(imInputHSV, vMinHSV, vMaxHSV)
+
+	imYellowBinary = imYellow / 255
+
+	# 対象色エリア（最も縦に長いもの）の水平位置の割り出し
+	vSumYellowVertical = np.sum(imYellowBinary, axis=0)
+	sMaxIndex =vSumYellowVertical.argmax()
+
+	# 対象色エリアの縦の長さが5画素よりも大きい場合、ターゲットに設定
+	if vSumYellowVertical[sMaxIndex] > 5:
+		sHorizontal = sMaxIndex
+		sVertical = -1
+		sSize = vSumYellowVertical[sMaxIndex]
+	else:
+		sHorizontal = -1
+		sVertical = -1
+		sSize = -1
+
+	return (sHorizontal, sVertical, sSize), imYellowBinary
