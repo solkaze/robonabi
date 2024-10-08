d94e16c1ac588de972666352c78dc5c286120610
solkaze <t.yoichi.0805@gmail.com>
2024-10-09 01:20:10 +0900

    青色のターゲット設定を追加

--- locateTarget.py
+++ locateTarget.py
@@ -59,6 +59,32 @@ def locateEnemy(imInputHSV):
 
     return (sHorizontal, sVertical, sSize), imGreenBinary
 
+def locateTower(imInputHSV):
+    # 対象物(青)
+    vMinHSV = np.array([90, 180, 0])
+    vMaxHSV = np.array([130, 255, 255])
+    imBlue = cv2.inRange(imInputHSV, vMinHSV, vMaxHSV)
+    
+    imBlueBinary = imBlue / 255
+
+    # 対象色エリア（最も縦に長いもの）の水平位置の割り出し
+    vSumBlueVertical = np.sum(imBlueBinary, axis=0)
+    sMaxIndex = vSumBlueVertical.argmax()
+
+    # 対象色エリアの縦の長さが5画素よりも大きい場合、ターゲットに設定
+    if vSumBlueVertical[sMaxIndex] > 5:
+        sHorizontal = sMaxIndex
+        sVertical = -1
+        sSize = vSumBlueVertical[sMaxIndex]
+    else:
+        sHorizontal = -1
+        sVertical = -1
+        sSize = -1
+
+    return (sHorizontal, sVertical, sSize), vSumBlueVertical
+
+    return flag
+
 def locateGoal(imInputHSV):
     # 対象物(黄色)
     vMinHSV = np.array([20,180,0])
